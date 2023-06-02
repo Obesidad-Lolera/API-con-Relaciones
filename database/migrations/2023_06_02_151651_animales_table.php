062896b517927088b0e52beb2188404b91d3710d
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-02 12:20:26 -0300

    Agregada migracion para crear tabla

---
+++ database/migrations/2023_06_02_151651_animales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AnimalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animales', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre");
+            $table->integer("cantidad_de_patas");
+            $table->string("especie");
+            $table->boolean("cola");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
